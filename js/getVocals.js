79e4f72b06c9d5b0f61349a96472ec83c9d282ab
tizzifona <tizzifona@gmail.com>
2024-10-19 23:57:58 +0200

    Fixed output of result of getVocals

--- js/getVocals.js
+++ js/getVocals.js
@@ -1,15 +1,13 @@
 export function getVocals(sentence) {
-    const vocals = ['a', 'e', 'i', 'o', 'u'];
-    let result = '';
-    
+    const vocals = ['a', 'e', 'i', 'o', 'u']; 
+    const result = []; 
+
     for (let i = 0; i < sentence.length; i++) {
-        const letter = sentence[i].toLowerCase();
-        if (vocals.includes(letter)) {
-            result += letter;
-            if (i < sentence.length - 1 && vocals.includes(sentence[i + 1].toLowerCase())) {
-                result += ',';
-            }
+        const letter = sentence[i].toLowerCase(); 
+        if (vocals.includes(letter) && !result.includes(letter)) { 
+            result.push(letter); 
         }
     }
-    return result;
+    
+    return result.join(', '); 
 }
